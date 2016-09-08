ddaafa23dfa8ca657ac5bb7930d8fee5c10b0475
mkyeong <mkyeong.lee@gmail.com>
2016-09-08 17:33:09 +0900

    More closed forest
    Added more trees - It already is a billboard trick

---
+++ unity_tests/Forest_layout/Assets/ArrowMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowMove : MonoBehaviour {
+    public float Walkspeed = 1.0f;
+    public float Rotatespeed = 15.0f;
+
+    // Use this for initialization
+    void Start () {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, Rotatespeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, -Rotatespeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0,0, -Walkspeed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 0, Walkspeed * Time.deltaTime));
+        }
+    }
+
+}
